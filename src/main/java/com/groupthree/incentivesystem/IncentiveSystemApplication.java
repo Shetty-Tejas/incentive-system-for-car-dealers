5ff5c71c98772c1f2ecc5b9e2edd8216936f1a96
Tejas Shetty <tejas-shetty@outlook.com>
2020-12-28 15:32:25 +0530

    Added Annotations

--- src/main/java/com/groupthree/incentivesystem/IncentiveSystemApplication.java
+++ src/main/java/com/groupthree/incentivesystem/IncentiveSystemApplication.java
@@ -2,8 +2,14 @@ package com.groupthree.incentivesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan({"com.groupthree.incentivesystem", "com.traineemanagementsystem.services"})
+@EntityScan("com.groupthree.incentivesystem.entities")
+@EnableJpaRepositories("com.groupthree.incentivesystem.repositories")
 public class IncentiveSystemApplication {
 
 	public static void main(String[] args) {
